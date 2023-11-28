b206b7f39a3fb5dce0adc4aa8192b7724347da49
Angus Robertson <angus@Anguss-iMac.lan>
2023-11-28 19:39:31 +1100

    added basic pmtiles function and transmission lines

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,10 @@
 import maplibregl, { StyleSpecification } from 'maplibre-gl';
 import { Ref, useCallback, useEffect, useRef, useState } from 'react';
-import { FullscreenControl, Map as MapGL, MapRef, NavigationControl, ScaleControl } from 'react-map-gl/maplibre';
+import { FullscreenControl, Layer, Map as MapGL, MapRef, NavigationControl, ScaleControl, Source } from 'react-map-gl/maplibre';
+import { Protocol } from 'pmtiles';
+
+const tilesUrl = `pmtiles://${new URL('../assets/infrastructure.pmtiles', import.meta.url).href}`;
+console.log(tilesUrl);
 
 export const ProjectMap: React.FC<{}> = ({}): React.JSX.Element => {
     const [mapStyle, setMapStyle] = useState<StyleSpecification | undefined>({
@@ -18,6 +22,9 @@ export const ProjectMap: React.FC<{}> = ({}): React.JSX.Element => {
 
     useEffect( () => {
         setDefaultStyle();
+
+        maplibregl.addProtocol('pmtiles', new Protocol().tile);
+        return () => { maplibregl.removeProtocol('pmtiles')};
     }, []);
 
     const mapReference = useCallback( (mapRef: MapRef) => {
@@ -43,7 +50,24 @@ export const ProjectMap: React.FC<{}> = ({}): React.JSX.Element => {
         >
 
             {/* LAYERS */}
-        
+            <Source id='infrastructure' type='vector' url={tilesUrl}>
+                <Layer
+                    id='transmission-lines'
+                    source-layer='transmission'
+                    type='line'
+                    minzoom={0}
+                    paint={{
+                        'line-color': '#B54EB2',
+                        'line-width': ['interpolate', ['linear'], ['zoom'], 2, 0.5, 10, ['interpolate', ['linear'], ['coalesce', ['get', 'voltage'], 0], 0, 1, 100, 1.8, 800, 4]],
+                        'line-opacity': ['interpolate', ['linear'], ['zoom'], 4, 0.6, 8, 1]
+                    }}
+                    layout={{
+                        'line-join': 'round',
+                        'line-cap': 'round'
+                    }}
+                ></Layer>
+            </Source>
+
             {/* CONTROLS */}
             <FullscreenControl />
             <ScaleControl maxWidth={200} style={{borderRadius: '0px', backgroundColor: '#ffffff20'}} />
