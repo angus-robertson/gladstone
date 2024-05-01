a318a9789a2f6c59f36fb5d62e411ed5866f7646
angus-robertson <angus.robertson@outlook.com.au>
2024-05-01 17:47:15 +1000

    basic geojson

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { createSignal } from 'solid-js'
 import 'maplibre-gl/dist/maplibre-gl.css'
-import { Map, NavigationControl, ScaleControl } from 'solid-maplibre'
+import { Layer, Map, NavigationControl, ScaleControl, Source } from 'solid-maplibre'
+
 
 function App() {
-	const [zoom, setZoom] = createSignal<number>(4)
+	const [zoom] = createSignal<number>(4)
 
 	return (
 		<>
@@ -18,6 +19,52 @@ function App() {
 					zoom: zoom()
 				}}
 			>
+				<Source
+          source={{
+            type: "geojson",
+            data: "https://angus-robertson.github.io/netzeromap/data/projects.geojson"
+          }}
+					id="plants"
+					
+				>
+					<Layer
+          layer={{
+            type: "circle",
+						filter: [
+							'any',
+							['==', ['get', 'FuelCategory'], 'Renewable'],
+							[
+								'all',
+								['==', ['get', 'fuel_type'], 'Hydrogen'],
+								['==', ['get', 'state'], 'QLD']
+							]
+						],
+						paint: {
+							'circle-color': [
+								'case',
+								['==', ['get', 'fuel_type'], 'Solar'],
+								'#fff201',
+								['==', ['get', 'fuel_type'], 'Wind'],
+								'#4db802',
+								['==', ['get', 'fuel_type'], 'Bioenergy'],
+								'#39b1b7',
+								['==', ['get', 'fuel_type'], 'Battery storage'],
+								'#c43b3f',
+								['==', ['get', 'fuel_type'], 'Hydro'],
+								'#3548a0',
+								['==', ['get', 'fuel_type'], 'Hydrogen'],
+								'#08f70a',
+								'#000000'
+							],
+							'circle-opacity': 0.5,
+							'circle-stroke-color': 'black',
+							'circle-stroke-width': 0.5,
+							'circle-stroke-opacity': 0.6
+						}
+          }}				
+					/>
+				</Source>
+
 				<ScaleControl />
 				<NavigationControl />
 			</Map>
